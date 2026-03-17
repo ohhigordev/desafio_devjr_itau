d5fc4ccc0ee85d1c2a89bd40ac2eef7cec168095
ohhigordev <160289988+ohhigordev@users.noreply.github.com>
2026-03-17 14:24:50 -0300

    docs: initial projects documentation

---
+++ src/test/java/Desafio_Itau_Dev_jr/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafio_Itau_Dev_jr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
